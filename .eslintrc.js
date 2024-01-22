3f19c49470d1918dd8439108585bedbfe3431a7e
Sai Sankeerth <sanpj2292@github.com>
2024-01-22 18:08:19 +0530

    chore: standard formatting
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,24 @@ module.exports = {
     es2021: true,
     commonjs: true,
   },
-  extends: ["prettier", "eslint:recommended", "plugin:json/recommended"],
+  extends: ['eslint:recommended', 'plugin:json/recommended', 'prettier'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'no-undef': 'warn',
+    'no-case-declarations': 'warn',
+    // note you must disable the base rule
+    // as it can report incorrect errors
+    'no-unused-vars': [
+      'warn', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'no-useless-escape': 'warn',
+  },
 };
--- loadingCode.js
+++ loadingCode.js
@@ -3,31 +3,29 @@
   rudderanalytics = window.rudderanalytics = [];
 
   var methods = [
-    "load",
-    "page",
-    "track",
-    "identify",
-    "alias",
-    "group",
-    "ready",
-    "reset",
-    "getAnonymousId",
-    "setAnonymousId",
+    'load',
+    'page',
+    'track',
+    'identify',
+    'alias',
+    'group',
+    'ready',
+    'reset',
+    'getAnonymousId',
+    'setAnonymousId',
   ];
 
   for (var i = 0; i < methods.length; i++) {
     var method = methods[i];
     rudderanalytics[method] = (function (methodName) {
       return function () {
-        rudderanalytics.push(
-          [methodName].concat(Array.prototype.slice.call(arguments))
-        );
+        rudderanalytics.push([methodName].concat(Array.prototype.slice.call(arguments)));
       };
     })(method);
   }
-  rudderanalytics.load("writeKey", "https://dataPlaneUrl", {
-    configUrl: "configBackendUrl",
-    logLevel: "DEBUG",
+  rudderanalytics.load('writeKey', 'https://dataPlaneUrl', {
+    configUrl: 'configBackendUrl',
+    logLevel: 'DEBUG',
   });
   // console.log("end of loader script");
 })();
