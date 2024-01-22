d908256bd85a3f8c5165b745f1a6ebbeb852663c
Sai Sankeerth <sanpj2292@github.com>
2024-01-22 18:11:23 +0530

    chore: eslint comment updates & remove ignoring .vscode
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
   rules: {
     'no-undef': 'warn',
     'no-case-declarations': 'warn',
-    // note you must disable the base rule
-    // as it can report incorrect errors
     'no-unused-vars': [
       'warn', // or "error"
       {
