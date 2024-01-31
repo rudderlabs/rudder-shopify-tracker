560e9ad212265a9c83f8cbbd98c12eecaac5a6e4
Sai Sankeerth <sanpj2292@github.com>
2024-01-31 15:57:06 +0530

    chore: update sonarcloud related reports, pull request template
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ const config = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'reports/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['/node_modules/'],
